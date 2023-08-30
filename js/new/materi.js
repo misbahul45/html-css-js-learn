e9d4b920d515b550a4cdfcb1ad2a7008cc0f6fd4
misbahul45 <misbahulmuttaqin395@gmail.com>
2023-08-30 14:31:10 +0700

    memperbarui materi

--- js/new/materi.js
+++ js/new/materi.js
@@ -1,2 +1,177 @@
 //=>fisr code console.log("welcomeback My Self")
-console.log("i will react js")
+//console.log("i will learn reactjs and nextjs just 4 month")
+
+//variabel=> var,let and const
+//var is not recomend because is not suported block scope
+//let will be use if you want to change the value of variabel in future
+// const will be use if the value of variabel is constan
+ 
+// var simbol="*********"
+// let simple="contoh 1"
+// var simbol="*********"//this is not eror because use var so you can declaration more and more so its not recomend
+// simple="contoh 2"
+// const contoh="contoh 1"
+// var simbol="*********"
+// contoh="contoh 3"//+> this will be eror because use const key
+
+// const name="Misbahul Muttaqin";
+// const whatDoYoWantInYourLife="I"m Just become a good people in hablumminallah dan hablumminannas";
+// const gender="Male";
+// const twitter="I dont have";
+// console.log(name)
+// console.log(whatDoYoWantInYourLife)
+// console.log(gender)
+// console.log(twitter)
+
+
+//=>number in javaScript
+// let num=10;
+// console.log(num);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log((2**10));
+// num++;
+// console.log(num)
+// num--;
+// console.log(num)
+
+// const num1=12;
+// const num2=2;
+// const add=num1+num2
+// console.log(num1," + ",num2,"hasil :",add);
+// const minus=num1-num2;
+// console.log(num1," + ",num2,"hasil :",minus);
+// const multiply=num1*num2
+// console.log(num1," + ",num2,"hasil :",multiply);
+// const substring=num1/num2
+// console.log(num1," + ",num2,"hasil :",substring);
+// const modulo=num1%num2;
+// console.log(num1," + ",num2,"hasil :",modulo);
+// const eksponen=num2**num1;
+// console.log(num1," + ",num2,"hasil :",eksponen);
+
+//variabel boolean =>true,false true=1 and false=0
+//=>comparasion 
+// console.log(10>2);true
+// console.log(10<3);false
+// console.log(20<=20);true
+// console.log("aku"=="aku");true
+// console.log("aku"!="aku");false
+// console.log(1=="1");true
+// console.log(1==="1");false
+
+// //=>string
+// const firstName="Misbahul";
+// const secondName="Muttaqin";
+// console.log(firstName);
+// console.log(`Fisrt Name : ${firstName}`)
+// console.log(`second Name : ${secondName}`)
+// console.log(secondName)
+// //concatenation 
+// const fullName=firstName+" "+secondName;
+// console.log(fullName)
+// console.log(`full Name : ${fullName}`)
+// //concatenation =>use .concat()
+// console.log("************")
+// console.log(firstName.concat(" ",secondName))
+// //append string => +=
+// console.log("************")
+// let sayHelo="halo";
+// sayHelo+=" "+firstName;
+// console.log(sayHelo);
+// //length
+// console.log("************")
+// console.log(firstName.length);
+// console.log(firstName.indexOf("i"))
+// //uppercas and lower case
+// console.log("************")
+// console.log(firstName.toUpperCase());
+// console.log(firstName.toLowerCase());
+// //slice
+// console.log("************");
+// let copyAllFisrtName=firstName.slice(0);
+// console.log(copyAllFisrtName);
+// console.log(copyAllFisrtName.slice(0,2));//Mi=>(index,length);
+// //split and join
+// console.log("************");
+// let arrayCopy=copyAllFisrtName.split("");
+// console.log(arrayCopy);
+// console.log(fullName.split(" "));
+// console.log(arrayCopy.join(""))
+// console.log(arrayCopy.join("-"))
+// //include
+// console.log(firstName.includes("s"));//true
+// console.log(firstName.includes("m"));//false
+// //trim
+// let newname="     xnineTzy  ";
+// console.log(newname);
+// console.log(newname.trim())
+
+// const num=50;//number
+// const numString="50";//string
+// console.log(typeof(num))
+// console.log(typeof(numString))
+// const addNum=num+Number(numString);//Number(var) casting to Number
+// console.log(addNum)
+///=>swicth and if else
+// let time=9;
+// let greeting;
+// if(time<10){
+//     greeting="good Morning";
+//     console.log(greeting);
+// }else if(time<20){
+//     greeting="good day";
+//     console.log(greeting)
+// }else{
+//     greeting="good evening";
+//     console.log(greeting)
+// }
+// let pw=9;
+//     if(pw===8){
+//         console.log("welcome")
+//     }else if(pw<=8){
+//         console.log("password is to short")
+//     }else if(pw>=8){
+//         console.log("to long pw")
+//     }else{
+//         console.log("please provide the pw")
+//     }
+
+
+    // let operators="+";
+    // const num1=10;
+    // const num2=20;
+
+    // switch(operators){
+    //     case "+":
+    //         console.log(num1+num2)
+    //     break;
+    //     case "-":
+    //         console.log(num1-num2)
+    //     break;
+    // }
+
+// //=>for loop
+// for(let i=1;i <=5;i++ ){
+//     for(let x=1;x<i;x++){
+//        console.log(x)
+//         if(x%i==0){
+//             console.log(x +"\n")
+//         }
+//     } 
+// }
+
+
+// for(let i=0;i<=1000;i++){
+//     console.log("hello misbahul ",i)
+// }
+
+let i=1
+while(i<=5){
+//     console.log("hello word",i)
+//     i++;
+// }
+
